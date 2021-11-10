21a3d1a7d04c6e0465b75fc63edd4d165d970c19
saikatsingha <saikatsingha@gmail.com>
2021-11-11 01:14:52 +0530

    Template using bootstrap

--- twitter/tweets/views.py
+++ twitter/tweets/views.py
@@ -3,12 +3,15 @@ from django.shortcuts import render
 
 from .models import Tweet
 
+# Create your views here.
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     """
-    REST API VIEW    
+    REST API VIEW
+    Consume by JavaScript or Swift/Java/iOS/Andriod
+    return json data
     """
     data = {
         "id": tweet_id,
@@ -20,4 +23,4 @@ def tweet_detail_view(request, tweet_id, *args, **kwargs):
     except:
         data['message'] = "Not found"
         status = 404
-    return JsonResponse(data, status=status) 
+    return JsonResponse(data, status=status) # json.dumps content_type='application/json'

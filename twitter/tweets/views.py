5968f6ea1e7c346bc6f9d246b30eaab7bf3caacb
saikatsingha <saikatsingha@gmail.com>
2022-04-15 00:18:57 +0530

    tweet data showing using Json

--- twitter/tweets/views.py
+++ twitter/tweets/views.py
@@ -1,3 +1,28 @@
+from os import stat
 from django.shortcuts import render
+from django.http import HttpResponse, Http404, JsonResponse
+
+from .models import Tweet
 
 # Create your views here.
+
+def home_view(request, *args, **kwargs):
+    return HttpResponse('<h1>Hello World</h1>')
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    data ={
+        "id": tweet_id,
+        # "content": obj.content,
+        # "image_path": obj.image.url
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data["content"] = obj.content
+    except:
+        data["message"] = "Not Found"
+        status = 404
+
+    
+    # return HttpResponse(f'<h1>Hello {tweet_id} - {obj.content}</h1>')
+    return JsonResponse(data, status= status)
--- twitter/twitter/settings.py
+++ twitter/twitter/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'tweets',
 ]
 
 MIDDLEWARE = [
@@ -73,12 +75,27 @@ WSGI_APPLICATION = 'twitter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'twitter',
+        'USER': 'saikat123',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '',
     }
+
 }
+
+
 
 
 # Password validation
